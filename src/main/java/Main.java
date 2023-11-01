8e735691cdd509bf01fd089c18e8c0f77218d53b
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-01 18:08:25 +0300

    Mortgage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        final int APARTMENT_PRICE_RUB = 5000000;
-        final double PERCENT_FIRST_PAYMENT = 0.3;
-        final double AMOUNT_FIRST_PAYMENT = APARTMENT_PRICE_RUB * PERCENT_FIRST_PAYMENT;
+        final int apartmentPriceRub = 5000000;
+        final double percentFirstPayment = 0.3;
+        final double amountFirstPayment = apartmentPriceRub * percentFirstPayment;
 
-        System.out.println("Сумма первоначального взноса: " + AMOUNT_FIRST_PAYMENT);
+        System.out.println("Сумма первоначального взноса: " + amountFirstPayment);
     }
 }
